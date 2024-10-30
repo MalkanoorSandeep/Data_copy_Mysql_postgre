93377787f49033f121ac4b5d53024bdd23934fda
MalkanoorSandeep <sandeepmalkanoor@Sandeeps-MacBook-Pro.local>
2024-10-30 00:59:39 -0400

    Refactored project structure, added db_connection, updated main and config files

--- config.py
+++ config.py
@@ -1,13 +1,28 @@
 import os
 from dotenv import load_dotenv
+from mysql.connector import DatabaseError, Error
 from loguru import logger
+import getpass
 
-load_dotenv()
+
+db_env = os.getenv("environment", "dev")
+load_dotenv(f"/Users/sandeepmalkanoor/Documents/Python/Project1/.env.{db_env}")
+   #/Users/sandeepmalkanoor/Documents/Python/Project1/.env.dev_dat_lemur
+
+   
 MYSQL_CONFIG = {
-    'host_name': os.getenv('MYSQL_HOST'),         # MySQL server host
-    'user_name': os.getenv('MYSQL_USER'),     # MySQL username
-    'password': os.getenv('MYSQL_PASSWORD'), # MySQL password
+    'host': os.getenv('MYSQL_HOST'),         # MySQL server host
+    'user': os.getenv('MYSQL_USER'),        # MySQL username
+    'password': None,                       # MySQL password
     'database': os.getenv('MYSQL_DATABASE')  # MySQL database name
 }
 
+# Prompt for username if it's not provided in environment variables
+if MYSQL_CONFIG['user'] is None:
+    MYSQL_CONFIG['user'] = input("Enter MySQL username: ")
 
+# Always prompt for password at runtime if not set
+MYSQL_CONFIG['password'] = getpass.getpass("Enter MySQL password: ")
+
+# Log final MYSQL_CONFIG to check if values were updated
+logger.info(f"Connected to the database: {MYSQL_CONFIG['database']}")
---
+++ db_connection.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import Error, DatabaseError
+from config import MYSQL_CONFIG
+from loguru import logger
+
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host = MYSQL_CONFIG['host'], 
+            username = MYSQL_CONFIG['user'], 
+            password = MYSQL_CONFIG['password'],
+            database = MYSQL_CONFIG['database']
+        )
+
+        if connection.is_connected():
+            return connection
+        
+
+    except DatabaseError as e:
+        # Specific error handling for an invalid database name
+        if e.errno == 1049:  # Error code for "Unknown database"
+            logger.error("Invalid database name specified or database not found.")
+        else:
+            logger.error(f"Database error: {e}")
+        return None
+
+
+    except Error as e:
+        logger.error(f"Database conncetion error: {e}")
+        return None
