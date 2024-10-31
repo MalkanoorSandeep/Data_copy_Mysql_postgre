c73565084fb1b603cee0cc42e382b437fe48d20c
MalkanoorSandeep <sandeepmalkanoor@Sandeeps-MacBook-Pro.local>
2024-10-30 21:18:05 -0400

    Add function to fetch record counts from the tables

--- main.py
+++ main.py
@@ -3,16 +3,27 @@ from loguru import logger
 import pandas as pd
 import os
 
-def my_sql_conncetion_logic():
-     connection = get_db_connection()
+def listing_tables():
      if connection:
             cursor = connection.cursor()
             cursor.execute("show tables")
             table_list = cursor.fetchall()
             logger.info(f"Total tables found: {len(table_list)}")
-            logger.info(f"Tables {[table[0] for table in table_list]}")
+            table_names = [table[0] for table in table_list]
+            logger.info(f"Tables are: {table_names}")
+            return table_names
 
+def record_count(tables):
+     cursor = connection.cursor()
+     logger.info(f"The number of records present in each table:")
+     for table in tables:
+        query = f"SELECT COUNT(*) FROM `{table}`"
+        cursor.execute(query)
+        output = cursor.fetchone()
+        logger.info(f"{table} : {output[0]}")
+        
 
 if __name__ == "__main__":
-    my_sql_conncetion_logic()
-    
+    connection = get_db_connection()
+    tables = listing_tables()
+    record_count(tables)
